608e4d56c52e7f59bfb751ee06f39d61243bd818
Shelley Wong <shelleywong@Shelleys-MacBook-Pro.local>
2020-05-06 10:45:26 -0700

    updates check for initial piezas turn

--- PiezasTest.cpp
+++ PiezasTest.cpp
@@ -22,16 +22,9 @@ TEST(PiezasTest, sanityCheck)
 	ASSERT_TRUE(true);
 }
 
-TEST(PiezasTest, check_board_constructor)
+TEST(PiezasTest, check_board_reset)
 {
   Piezas piezas;
-  bool all_blank = true;
-  for(int i = 0; i < BOARD_HEIGHT; i++){
-    for(int j = 0; j < BOARD_WIDTH; j++){
-      if(piezas.board[i][j] != Blank){
-        all_blank = false;
-      }
-    }
-  }
-	ASSERT_TRUE(all_blank);
+  piezas.reset();
+	ASSERT_TRUE(piezas.turn == X);
 }
