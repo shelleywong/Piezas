5cc7f6a653767b88a0991c0f4ebe456d0699db3a
Shelley Wong <shelleywong@Shelleys-MacBook-Pro.local>
2020-05-06 11:07:03 -0700

    fixes column

--- Piezas.cpp
+++ Piezas.cpp
@@ -68,11 +68,11 @@ Piece Piezas::dropPiece(int column)
 Piece Piezas::pieceAt(int row, int column)
 {
   Piece current;
-  if(row < 0 || col < 0){
+  if(row < 0 || column < 0){
     current = Invalid;
     return current;
   }
-  if(row >= 3 || col >= 4){
+  if(row >= 3 || column >= 4){
     current = Invalid;
     return current;
   }
